b1488aee0b8e38d4a5d99aa5eab4bfa46cd968f5
Benjamin Kies <ben@joingrouper.com>
2014-02-06 17:21:05 -0500

    Refactored ship placement to be an object

--- players/random_seeker_player.rb
+++ players/random_seeker_player.rb
@@ -7,32 +7,7 @@ class RandomSeekerPlayer
   end
 
   def new_game
-    places = []
-    [5, 4, 3, 3, 2].each do |length|
-      p = place([:across, :down][rand(2)], length)
-      puts p
-      while conflict?(p, places)
-        p = place([:across, :down][rand(2)], length)
-      end
-      places << p
-    end
-    p "places #{places}"
-    places
-  end
-
-  def conflict?(p, places)
-    taken = places.map { |p| ship_dots(p) }.flatten(1)
-    p taken
-    ship_dots(p).any? { |d| taken.include?(d) }
-  end
-
-  def ship_dots(ship)
-    size = ship[2] - 1
-    if ship[3] == :across
-      (0..size).map { |x| [ship[0] + x, ship[1]] }
-    else
-      (0..size).map { |x| [ship[0], ship[1] + x] }
-    end
+    ShipPlacer.new.place_all
   end
 
   def take_turn(state, ships_remaining)
@@ -40,12 +15,67 @@ class RandomSeekerPlayer
     @board.update(state, ships_remaining)
     @board.take_turn
   end
+end
 
-  def place(dir, size)
-    if dir == :across
-      [rand(10-(size +1)), rand(10), size, dir]
-    elsif dir == :down
-      [rand(10), rand(10-(size +1)), size, dir]
+# ShipPlacer
+#
+# A class that generates a list of placed ships
+#
+#
+# Currently this is done mostly randomly. From biggest ship to smallest,
+# Place the ship randomly on the board. If it conflicts with an existing
+# one, then place it again.
+#
+#
+class ShipPlacer
+
+  def initialize
+    @placements = []
+  end
+
+  def place_all
+    ships.each do |ship|
+      begin
+        p = generate_placement([:across, :down][rand(2)], ship)
+      end while conflict?(p)
+      @placements << p
+    end
+    @placements
+  end
+
+  private
+
+  def ships
+    [5, 4, 3, 3, 2]
+  end
+
+  # Does this ship placement conclict with the ships already placed?
+  def conflict?(placement)
+    placement_dots(placement).any? { |d| taken_dots.include?(d) }
+  end
+
+  # The dots currently taken by other ships
+  def taken_dots
+    @placements.map {|p| placement_dots(p) }.flatten(1)
+  end
+
+  # Choose ship placement
+  def generate_placement(direction, ship)
+    if direction == :across
+      [rand(10-(ship +1)), rand(10), ship, direction]
+    elsif direction == :down
+      [rand(10), rand(10-(ship +1)), ship, direction]
+    end
+  end
+
+  # The dots taken up by a ship
+  def placement_dots(placement)
+    x, y, ship, orientation = placement
+    size = ship -1
+    if orientation == :across
+      (0..size).map { |i| [x + i, y] }
+    else
+      (0..size).map { |i| [x, y + i] }
     end
   end
 end
