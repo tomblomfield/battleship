5e6b77f614eef582499502d6f9b6807180fefeb2
Robert Lord <robert@lord.io>
2014-02-10 16:27:41 -0500

    Refactor some bits of code

--- contestants/hacky.rb
+++ contestants/hacky.rb
@@ -19,45 +19,47 @@ class HackyPlayer
       [2, 2, 3, :across],
       [9, 7, 2, :down]
     ]
-
-
   end
 
 
   def take_turn(state, ships_remaining)
     @state = state
     # state is the known state of opponents fleet
+
     # ships_remaining is an array of the remaining opponents ships
 
-
-    # @hunt = false if state[@guess[1]][@guess[0]] == :hit
+    @hunt = false if check_state(@guess[0], @guess[1]) == :hit
 
     if @hunt
-      begin
-        @guess = hunt_guess
-      end while self.state?(@guess[0], @guess[1]) != :unknown
+      @guess = hunt_guess
     else
-
+      @guess = seek_guess
     end
-
     @guess
   end
 
-  # you're free to create your own methods to figure out what to do next
+
+  private
 
   def hunt_guess
-    x = rand(10)
+    begin
+      x = rand(10)
 
-    if x.even?
-      y = rand(5) * 2
-    else
-      y = (rand(5) * 2) + 1
-    end
+      if x.even?
+        y = rand(5) * 2
+      else
+        y = (rand(5) * 2) + 1
+      end
+      guess = [x, y]
+    end while check_state(guess[0], guess[1]) != :unknown
 
-    [x, y]
+    guess
   end
 
-  def state?(x, y)
+  def seek_guess
+  end
+
+  def check_state(x, y)
     return :outside unless (0..9).cover? x
     return :outside unless (0..9).cover? y
     @state[y][x]
