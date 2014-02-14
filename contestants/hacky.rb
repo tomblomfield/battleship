0584a5ca13c2f88a6ee4967a2a0d5923d06908cf
Robert Lord <robert@lord.io>
2014-02-14 15:23:45 -0500

    Add probability calculator

--- contestants/hacky.rb
+++ contestants/hacky.rb
@@ -28,17 +28,28 @@ class HackyPlayer
 
     # ships_remaining is an array of the remaining opponents ships
 
-    if @guess && check_state(@guess[0], @guess[1]) == :hit
-      @hunt = false
-      @current_hit = @guess
+    # if @guess && check_state(@guess[0], @guess[1]) == :hit
+    #   @hunt = false
+    #   @current_hit = @guess
+    # end
+
+    # if @hunt
+    #   @guess = hunt_guess
+    # else
+    #   @guess = seek_guess
+    # end
+    map = remap_probabilty
+    highest_hit_coords = nil
+    highest_hit_prob = 0.0
+    map.each do |coords, prob|
+      if highest_hit_prob < prob
+        highest_hit_prob = prob
+        highest_hit_coords = coords
+      end
     end
 
-    if @hunt
-      @guess = hunt_guess
-    else
-      @guess = seek_guess
-    end
-    @guess
+    @guess = highest_hit_coords
+    highest_hit_coords
   end
 
 
@@ -60,6 +71,47 @@ class HackyPlayer
   end
 
 
+  def remap_probabilty
+    map = {}
+    hits = []
+    unknown = []
+    misses = []
+
+    each_square do |x, y|
+      case check_state x, y
+      when :unknown
+        unknown << [x, y]
+      when :hit
+        hits << [x, y]
+      when :miss
+        misses << [x, y]
+      end
+    end
+
+    each_square do |x, y|
+      map[[x,y]] = (17.0 - hits.length) / unknown.length
+    end
+
+    unknown.each do |u|
+      adjacent_statuses = adjacent_states(u).reject do |adj|
+        check_state(adj[0], adj[1]) == :miss
+      end
+
+      map[u] = 0 if adjacent_statuses.length == 0
+    end
+
+    misses.each do |miss|
+      map.delete miss
+    end
+
+    hits.each do |hit|
+      map.delete hit
+    end
+
+    puts map
+    sleep 1
+    map
+  end
 
   def seek_guess
     adjacents = adjacent_states(@current_hit)
@@ -75,6 +127,14 @@ class HackyPlayer
       hunt_guess
     end
 
+  end
+
+  def each_square
+    10.times do |x|
+      10.times do |y|
+        yield x, y
+      end
+    end
   end
 
 
